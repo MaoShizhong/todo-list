35c0cdcfab1231e79f9fe0daeb229591cd793b06
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-05-03 03:19:41 +0100

    Add modal button with open/close

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const openModal = document.querySelector('#open-modal');
+const closeModal = document.querySelector('#close-modal');
+const modal = document.querySelector('#modal');
+
+openModal.addEventListener('click', () => modal.showModal());
+closeModal.addEventListener('click', () => modal.close());
